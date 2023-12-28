46c06a8bf4796c4eb0e698dda340a5805a2764a3
xxelectron <me@florian-s.de>
2023-12-28 15:40:33 +0100

    movement

--- app.mjs
+++ app.mjs
@@ -9,13 +9,15 @@ import { MapItem } from './elements/map.mjs';
 import { VRButton } from './jsm/webxr/VRButton.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import * as LEVER from './elements/button.mjs';
+import { createVRcontrollers } from './elements/controlls.mjs';
 
-const MOVESCALE = 0.001;
+const MOVESCALE = 0.01;
 let camerapos = { x: 0, y: 1.6, z: 0 };
 let vr = false;
 let experimental = false;
 let playerStartPos = { x: 0, y: 0, z: 0 };
 let camera = undefined;
+let controllerOne, controllerTwo;
 let offset = { x: 15, y: 0, z: 10 };
 //New scene
 let scene = new THREE.Scene();
@@ -45,8 +47,27 @@ document.addEventListener('keydown', (e) => {
    }
 })
 
+//axes, buttons, hapticActuators
+//ControllerOne = right
+//buttons -> 0 -> pressed/touched/value -> Button Trigger
+//buttons -> 1 -> pressed/touched/value -> Button Grip
+//buttons -> 3 -> pressed/touched/value -> Button Stick Press
+//buttons -> 4 -> pressed/touched/value -> Button A
+//buttons -> 5 -> pressed/touched/value -> Button B
 
+//buttons -> 4 -> pressed/touched/value -> Button X
+//buttons -> 5 -> pressed/touched/value -> Button Y
 function updateVRCamera() {
+   if (vr) {
+      let rotation = renderer.xr.getCamera().rotation;
+      console.log(rotation);
+      let coords = controllerOne.gamepad.axes;
+      //console.log(coords);
+
+      offset.x -= coords[2] * MOVESCALE;
+      offset.z -= coords[3] * MOVESCALE;
+      //Check if buttons pressed
+   }
    totalmaze.position.x = offset.x;
    totalmaze.position.y = offset.y;
    totalmaze.position.z = offset.z;
@@ -58,6 +79,17 @@ renderer.xr.addEventListener('sessionstart', () => {
       renderer.xr.getCamera().position.set(1, 1.6, 1);
    } else {
       console.log("FOUND CAMERA");
+      //let { controller1, controller2 } = createVRcontrollers(scene, renderer, (controller, eventdata) => { console.log("Done") })
+
+      controllerOne = renderer.xr.getController(0);
+      controllerOne.addEventListener('connected', (e) => {
+         controllerOne.gamepad = e.data.gamepad;
+      });
+      controllerTwo = renderer.xr.getController(1);
+      controllerTwo.addEventListener('connected', (e) => {
+         controllerTwo.gamepad = e.data.gamepad;
+      });
+      vr = true;
    }
 });
 
@@ -79,7 +111,7 @@ window.onload = function () {
    scene.add(mesh);
 
    //Player
-   let playerinfo = USER.CreatePlayer(scene, playerStartPos, { x: 0, y: 0, z: 0 }, renderer);
+   //let playerinfo = USER.CreatePlayer(scene, playerStartPos, { x: 0, y: 0, z: 0 }, renderer);
    //camera = playerinfo.camera;
    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
    camera.position.set(camerapos.x, camerapos.y, camerapos.z);
@@ -89,8 +121,7 @@ window.onload = function () {
    totalmaze.position.z = 10;
    //Models
    //OBJECTS.Load(scene);
-   let controls = new OrbitControls(camera, renderer.domElement);
-
+   //let controls = new OrbitControls(camera, renderer.domElement);
    renderer.shadowMap.enabled = true;
    renderer.shadowMap.type = THREE.PCFShadowMap;
 
@@ -101,7 +132,7 @@ window.onload = function () {
    document.body.appendChild(renderer.domElement);
    document.body.appendChild(VRButton.createButton(renderer));
 
-   renderer.xr.getCamera().position.copy(camera.position);
+   //renderer.xr.getCamera().position.copy(camera.position);
 
    function render() {
       updateVRCamera();
